c78b2dadbbbd0918e077cef6ecd9b4f3899034e6
Martin <7252614+Lhoerion@users.noreply.github.com>
2021-04-08 02:59:32 +0200

    Minor changes

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DocFx.Plugins.TypeScriptReference")]
+[assembly: AssemblyTitle("DocFx.Plugins.ExtractSearchIndex")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DocFx.Plugins.TypeScriptReference")]
+[assembly: AssemblyProduct("DocFx.Plugins.ExtractSearchIndex")]
 [assembly: AssemblyCopyright("Copyright © Microsoft, Lhoerion 2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
