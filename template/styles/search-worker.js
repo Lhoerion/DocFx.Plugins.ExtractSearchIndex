3f9ac07262231347cb6ddc85652419148f9841b9
Martin <7252614+Lhoerion@users.noreply.github.com>
2021-11-21 01:29:50 +0100

    Reformat code in search worker script

--- template/styles/search-worker.js
+++ template/styles/search-worker.js
@@ -1,45 +1,43 @@
-(function () {
-  importScripts('https://cdnjs.cloudflare.com/ajax/libs/lunr.js/2.3.9/lunr.min.js');
+self.importScripts('https://cdnjs.cloudflare.com/ajax/libs/lunr.js/2.3.9/lunr.min.js');
 
-  var lunrIndex;
+let lunrIndex;
 
-  var searchData = {};
+let searchData = {};
 
-  var searchDataRequest = new XMLHttpRequest();
-  var searchIndexRequest = new XMLHttpRequest();
+const searchDataRequest = new XMLHttpRequest();
+const searchIndexRequest = new XMLHttpRequest();
 
-  searchDataRequest.open('GET', '../index.json');
-  searchDataRequest.onload = function () {
-    if (this.status !== 200) {
-      return;
-    }
-    searchData = JSON.parse(this.responseText);
-
-    searchIndexRequest.send();
+searchDataRequest.open('GET', '../index.json');
+searchDataRequest.onload = function () {
+  if (this.status !== 200) {
+    return;
   }
+  searchData = JSON.parse(this.responseText);
 
-  searchIndexRequest.open('GET', '../search-index.json');
-  searchIndexRequest.onload = function () {
-    if (this.status !== 200) {
-      return;
-    }
-    lunrIndex = lunr.Index.load(JSON.parse(this.responseText));
+  searchIndexRequest.send();
+}
 
-    postMessage({ e: 'index-ready' });
+searchIndexRequest.open('GET', '../search-index.json');
+searchIndexRequest.onload = function () {
+  if (this.status !== 200) {
+    return;
   }
+  lunrIndex = lunr.Index.load(JSON.parse(this.responseText));
 
-  searchDataRequest.send();
+  postMessage({e: 'index-ready'});
+}
 
-  onmessage = function (oEvent) {
-    var q = oEvent.data.q;
-    var hits = lunrIndex.search(q.split(/\s+/g).map(term => {
-      return !term.startsWith('-') ? (!term.startsWith('+') ? '+' + term : term.substring(1)) : term;
-    }).join(' '));
-    var results = [];
-    hits.sort((a, b) => (searchData[a.ref].type > searchData[b.ref].type) - (searchData[a.ref].type < searchData[b.ref].type));
-    hits.forEach(function(hit) {
-      results.push(searchData[hit.ref]);
-    });
-    postMessage({ e: 'query-ready', q: q, d: results });
-  }
-})();
+searchDataRequest.send();
+
+onmessage = function (ev) {
+  const q = ev.data.q;
+  const hits = lunrIndex.search(q.split(/\s+/g).map(term => {
+    return !term.startsWith('-') ? (!term.startsWith('+') ? '+' + term : term.substring(1)) : term;
+  }).join(' '));
+  const results = [];
+  hits.sort((a, b) => (searchData[a.ref].type > searchData[b.ref].type) - (searchData[a.ref].type < searchData[b.ref].type));
+  hits.forEach(function (hit) {
+    results.push(searchData[hit.ref]);
+  });
+  self.postMessage({e: 'query-ready', q: q, d: results}, '*');
+}
